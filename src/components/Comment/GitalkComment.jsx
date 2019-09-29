e1ab52d7dc82624c27f4e9ed55a2889f2917235d
yangnianbing <137602812@qq.com>
2019-09-29 16:38:08 +0800

    gitalk

--- src/components/Comment/GitalkComment.jsx
+++ src/components/Comment/GitalkComment.jsx
@@ -8,7 +8,7 @@ class GitalkComment extends Component {
         var gitalk = new Gitalk({
             clientID: 'c22c8dc5a408526518ae',
             clientSecret: 'e375364b59350e1027178a93e1e8af885e937997',
-            repo: 'yangnianbing/comment',
+            repo: 'comment',
             owner: 'yangnianbing',
             admin: ['yangnianbing'],
             id: location.pathname,      // Ensure uniqueness and length less than 50
